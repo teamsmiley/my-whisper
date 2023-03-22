866b2beb0926e4ed06ee4c1bed990b77805121fd
brian_imac <teamsmiley@gmail.com>
2023-03-21 20:07:16 -0700

    asdf

--- app/main.py
+++ app/main.py
@@ -4,10 +4,8 @@ print("This line will be printed.")
 
 model = whisper.load_model("base")
 audio = model.transcribe("audio.mp3")
-audio = whisper.pad_or_trim(audio)
+# audio = whisper.pad_or_trim(audio)
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
-
-# detect the spoken language
 _, probs = model.detect_language(mel)
 print(f"Detected language: {max(probs, key=probs.get)}")
 
