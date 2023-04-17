f6b3944ff2cd31f5f07f28697b98bf73e985b6ee
brian_imac <teamsmiley@gmail.com>
2023-04-17 16:55:25 -0700

    update

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ import whisper
 
 print("This line will be printed.")
 
-model = whisper.load_model("base")
+model = whisper.load_model("large")
 result = model.transcribe("audio.mp3")
 print(result["text"])
