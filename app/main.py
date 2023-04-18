0ae22ca9393c30f41b7f3f99e56ba3086dd9dd48
brian_imac <teamsmiley@gmail.com>
2023-04-17 17:00:46 -0700

    update

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 import whisper
 
-print("This line will be printed.")
+import torch
 
-model = whisper.load_model("large")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("Using CPU")
+
+
+print("lets start read file ")
+
+model = whisper.load_model("large-v2")
 result = model.transcribe("audio.mp3")
 print(result["text"])
