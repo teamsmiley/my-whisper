117c493265f24f103a78a3a3a184ae468c98a4a8
brian_imac <teamsmiley@gmail.com>
2023-04-17 21:27:47 -0700

    update

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 import whisper
-import torch
+# import torch
 
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("Using GPU:", torch.cuda.get_device_name(0))
-else:
-    device = torch.device("cpu")
-    print("Using CPU")
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")
+#     print("Using GPU:", torch.cuda.get_device_name(0))
+# else:
+#     device = torch.device("cpu")
+#     print("Using CPU")
 
 print("lets start read file ")
 
