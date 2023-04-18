b57e4313dc54946fb6e016f32066515d9af5596a
brian_imac <teamsmiley@gmail.com>
2023-04-17 22:57:23 -0700

    update

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ git pull && docker-compose up --build
 
 - https://oomacorp.atlassian.net/wiki/spaces/DEV/pages/317654328/Enable+GPU+acceleration+with+container+services
 - https://oomacorp.atlassian.net/wiki/spaces/~61cbb2750586a20069f73371/pages/244057434/Implementing+Whisper+ASR+API
+
+## performance
+
+from 2:50 sec mp3 file
+
+| model    | model size | transcribe |
+| -------- | ---------- | ---------- |
+| tiny     | 72M        | 5 sec      |
+| base     | 140M       | 6 sec      |
+| small    | 460M       | 7 sec      |
+| medium   | 1.4G       | 13 sec     |
+| large-v2 | 2.9G       | 17 sec     |
--- app/main.py
+++ app/main.py
@@ -9,12 +9,12 @@ else:
     device = torch.device("cpu")
     print("Using CPU")
 
-print("lets load module")
-# model = whisper.load_model("tiny") # download 13 sec , transcribe: 5 sec
-# model = whisper.load_model("base") # download 8 sec , transcribe: 6.2 sec
-# model = whisper.load_model("small") # download 1:42 sec , transcribe: 7.5 sec
-# model = whisper.load_model("medium") # download 1:42 sec , transcribe: 13.5 sec
-model = whisper.load_model("large-v2") # download 1:42 sec , transcribe: 13.5 sec
+print("lets load module") 
+# model = whisper.load_model("tiny") 
+# model = whisper.load_model("base") 
+# model = whisper.load_model("small") 
+# model = whisper.load_model("medium") 
+model = whisper.load_model("large-v2") 
 start = time.time()
 result = model.transcribe("audio.mp3")
 print(result["text"])
