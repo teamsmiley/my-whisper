13d7b28b48959a405e3b551303475412dd1fa62b
brian_imac <teamsmiley@gmail.com>
2023-04-17 22:35:51 -0700

    update

--- app/main.py
+++ app/main.py
@@ -9,19 +9,15 @@ else:
     device = torch.device("cpu")
     print("Using CPU")
 
-model = "tiny"
-
 print("lets start read file ")
-
 start = time.time()
-
-model = whisper.load_model(model)
+model = whisper.load_model("tiny")
 # model = whisper.load_model("base")
 # model = whisper.load_model("small")
 # model = whisper.load_model("medium")
 # model = whisper.load_model("large-v2")
 result = model.transcribe("audio.mp3")
 print(result["text"])
-
 end = time.time()
-print(model + ": The time of execution of above program is :", (end-start) * 1000, "ms")
+print("The time of execution of above program is :",
+      (end-start) * 1000, "ms")
