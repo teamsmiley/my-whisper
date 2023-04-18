a2cda19f339a1499d68b3b2f6008a405e597ffc1
brian_imac <teamsmiley@gmail.com>
2023-04-17 22:30:03 -0700

    update

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ else:
 
 print("lets start read file ")
 
-model = whisper.load_model("tiny")
+# model = whisper.load_model("tiny")
 # model = whisper.load_model("base")
 # model = whisper.load_model("small")
-# model = whisper.load_model("medium")
+model = whisper.load_model("medium")
 # model = whisper.load_model("large-v2")
 result = model.transcribe("audio.mp3")
 print(result["text"])
