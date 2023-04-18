6c4afec911f5a6c415fe2838e943fe92c40996b6
brian_imac <teamsmiley@gmail.com>
2023-04-17 21:43:30 -0700

    update

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /app
 RUN apt update 
 RUN apt install ffmpeg -y
 RUN pip install -U openai-whisper
+RUN pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 COPY app/ /app/
 
 
--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 import whisper
-# import torch
+import torch
 
-# if torch.cuda.is_available():
-#     device = torch.device("cuda")
-#     print("Using GPU:", torch.cuda.get_device_name(0))
-# else:
-#     device = torch.device("cpu")
-#     print("Using CPU")
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("Using CPU")
 
 print("lets start read file ")
 
-# model = whisper.load_model("tiny")
+model = whisper.load_model("tiny")
 # model = whisper.load_model("base")
 # model = whisper.load_model("small")
 # model = whisper.load_model("medium")
-model = whisper.load_model("large-v2")
+# model = whisper.load_model("large-v2")
 result = model.transcribe("audio.mp3")
 print(result["text"])
