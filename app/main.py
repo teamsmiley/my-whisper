3fd3ca6a710a229f91323c60897a34a18ffbdf8c
brian_imac <teamsmiley@gmail.com>
2023-04-18 06:24:43 -0700

    update

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ start = time.time()
 
 # load audio and pad/trim it to fit 30 seconds
 audio = whisper.load_audio("audio-kr.mp3")
-# audio = whisper.pad_or_trim(audio)
+audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
 mel = whisper.log_mel_spectrogram(audio).to(model.device)
