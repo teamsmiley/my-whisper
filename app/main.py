663db0fa3c8c3e4f3e05435a6752c4769163b118
brian_imac <teamsmiley@gmail.com>
2023-04-21 19:36:05 -0700

    update

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import whisper
+import torch
+import time
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("Using GPU:", torch.cuda.get_device_name(0))
+    else:
+        device = torch.device("cpu")
+        print("Using CPU")
     return {"Hello": "World"}
 
