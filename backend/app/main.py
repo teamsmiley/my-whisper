973893f888d7da96c4081fbb07c9843575248bfe
brian_imac <teamsmiley@gmail.com>
2023-04-25 17:00:51 -0700

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,9 +26,6 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
 )
 
 SAMPLE_RATE=16000
