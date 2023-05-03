c00c7d56c88eb7f62526fa0556ab27d74aa9b8e8
brian_imac <teamsmiley@gmail.com>
2023-05-02 20:40:58 -0700

    update[skip-ci]

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  ws_url: 'ws://whisper-api.xgrid.dev/ws',
+  ws_url: 'wss://whisper-api.xgrid.dev/ws',
   ws_file_upload_url: 'https://whisper-api.xgrid.dev/asr',
 };
